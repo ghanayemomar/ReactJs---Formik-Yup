60d2aeaf1ed8a9bc9b600a2935e2dbc43b0f865a
ghanayemomar <o.ghanaiem@student.aaup.edu>
2023-09-17 20:08:45 +0300

    add error msg to the validation.

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -30,6 +30,7 @@ const BasicForm = () => {
         onBlur={handleBlur}
         className={errors.email && touched.email ? "input-error" : ""}
       />
+      {errors.email && touched.email && <p className="error">{errors.email}</p>}
 
       <label htmlFor="age">Age</label>
       <input
@@ -41,6 +42,7 @@ const BasicForm = () => {
         onBlur={handleBlur}
         className={errors.age && touched.age ? "input-error" : ""}
       />
+      {errors.age && touched.age && <p className="error">{errors.age}</p>}
 
       <label htmlFor="password">Password</label>
       <input
@@ -52,17 +54,26 @@ const BasicForm = () => {
         onBlur={handleBlur}
         className={errors.password && touched.password ? "input-error" : ""}
       />
+      {errors.password && touched.password && (
+        <p className="error">{errors.password}</p>
+      )}
 
-      <label htmlFor="ConfirmPassword">Confirm Password</label>
+      <label htmlFor="confirmPassword">Confirm Password</label>
       <input
-        id="ConfirmPassword"
+        id="confirmPassword"
         type="password"
         placeholder="Confirm password"
         value={values.confirmPassword}
         onChange={handleChange}
         onBlur={handleBlur}
-        className={errors.confirmPassword && touched.confirmPassword ? "input-error" : ""}
+        className={
+          errors.confirmPassword && touched.confirmPassword ? "input-error" : ""
+        }
       />
+      {errors.confirmPassword && touched.confirmPassword && (
+        <p className="error">{errors.confirmPassword}</p>
+      )}
+
       <button type="submit">Submit</button>
     </form>
   );
