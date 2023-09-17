d00c53f7bde593f4c92567b543ea837d6b8d4717
ghanayemomar <o.ghanaiem@student.aaup.edu>
2023-09-18 02:05:31 +0300

    adding select list

--- src/components/AdvancedForm.js
+++ src/components/AdvancedForm.js
@@ -1,6 +1,7 @@
 import { Formik, Form, Field } from "formik";
 import CustomInput from "./CustomInput";
 import { advancedSchema } from "../schemas";
+import CustomSelect from "./CustomSelect";
 const AdvancedForm = () => {
   return (
     <Formik
@@ -15,6 +16,18 @@ const AdvancedForm = () => {
             type="text"
             placeholder="Enter your username"
           />
+          <CustomSelect
+            label="Job Type"
+            name="jobType"
+            placeholder="Please select a job"
+          >
+            <option value="">Please select a job type</option>
+            <option value="developer">Developer</option>
+            <option value="designer">Designer</option>
+            <option value="manager">Manager</option>
+            <option value="other">Other</option>
+          </CustomSelect>
+
           <button type="submit">Submit</button>
         </Form>
       )}
